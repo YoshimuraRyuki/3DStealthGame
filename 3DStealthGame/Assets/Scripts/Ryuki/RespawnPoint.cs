979d85517f1126410d6b824e847bb4bab80f79f2
YoshimuraRyuki <yoshimura.ryuki.0224123@gmail.com>
2026-06-04 13:04:53 +0900

    プレイヤーリスポーン処理作成

---
+++ 3DStealthGame/Assets/Scripts/Ryuki/RespawnPoint.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RespawnPoint : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player1") || other.CompareTag("Player2"))
+        {
+            PlayerController playerRespawn = other.GetComponent<PlayerController>();
+
+            if (playerRespawn != null)
+            {
+                playerRespawn.SetRespawnPoint(transform);
+            }
+        }
+    }
+}
